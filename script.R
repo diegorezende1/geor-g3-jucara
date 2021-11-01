a5d13c0a42d8c4118d61aa45d7fd24835e9743fd
biancanogr <biancanogr@gmail.com>
2021-11-01 15:25:50 -0300

    atualizei o script e criei txt. com os gêneros selecionados

--- script.R
+++ script.R
@@ -9,3 +9,47 @@ here::set_here()
 # importar tabela
 
 
+#install packages (Bianca-01/11/2021)
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("here")
+library(here)
+install.packages("tidyr")
+library(tidyr)
+install.packages("dplyr")
+library(dplyr)
+
+
+#verificar diretório (Bianca-01/11/2021)
+getwd()
+
+#definir diretório de trabalho (Bianca-01/11/2021)
+setwd("C:/github/geor-g3-jucara/Atlantic_Ants-main/DATASET")
+
+#importar tabela, extraí o zip da pasta DATASET pra ter o txt. (Bianca-01/11/2021)
+#read.table("ATLANTIC_ANTS_dataset.txt", header = TRUE)
+
+# importar tabela e ler com tidyverse (Bianca-01/11/2021)
+ants <- readr::read_tsv(here::here("Atlantic_Ants-main", "DATASET", "ATLANTIC_ANTS_dataset.txt"))
+ants
+
+spec(ants)
+
+#selecionar colunas (Bianca-01/11/2021)
+
+ants_select <- ants %>%
+dplyr::select(Latitude.y, Longitude.x, Subfamily, Genus, Species, Start.year, Habitat.Type)
+ants_select
+
+#Selecionar os gêneros (Bianca-01/11/2021)
+ants_genus_select <- ants_select %>%
+dplyr::filter(Genus %in% c("Solenopsis", "Ectatomma", "Eciton"))
+ants_genus_select
+
+#Ordenar por ordem alfabética os gêneros (Bianca-01/11/2021)
+ants_arrange <- ants_genus_select %>%
+dplyr::arrange(Genus)
+
+#Exportar tabela com gêneros selecionados em ordem (Bianca-01/11/2021)
+write.table(ants_arrange, "AANTS_genus_selected.txt", 
+            row.names = FALSE, col.names = TRUE, quote = FALSE)
