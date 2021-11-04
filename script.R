2bb30853d5f9c519f7c282c9f3963a57352348f0
seu nome <mauricio.vancine@gmail.com>
2021-11-04 12:47:42 -0300

    alteracoes insanas antes do almoco - mauricio

--- script.R
+++ script.R
@@ -112,6 +112,9 @@ view(ants_sp_select)
 # criar geometria a partir dos dados de latlong (Luan 03-11)
 
 ants_sp_sf <- ants_sp_select %>% sf::st_as_sf(coords = c("Longitude.x", "Latitude.y"), crs = 4326)
+ants_sp_sf
+
+plot(ants_sp_sf$geometry, pch = 20)
 
 #=======
 
@@ -128,42 +131,62 @@ download.file(url = "https://biogeo.ucdavis.edu/data/worldclim/v2.1/fut/5m/wc2.1
 download.file(url = "https://biogeo.ucdavis.edu/data/worldclim/v2.1/fut/5m/wc2.1_5m_tmax_CanESM5_ssp585_2081-2100.zip",
               destfile = "worldcim/wc2.1_5m_tmax_CanESM5_ssp585_2081-2100.zip", mode = "wb")
 
+download.file(url = "https://biogeo.ucdavis.edu/data/worldclim/v2.1/base/wc2.1_5m_elev.zip",
+              destfile = "wc2.1_5m_elev.zip", mode = "wb")
+
 unzip("worldclim/wc2.1_5m_tmax.zip", exdir = "worldclim")
 unzip("worldclim/wc2.1_5m_tmax_CanESM5_ssp245_2081-2100.zip", exdir = "worldclim")
 unzip("worldclim/wc2.1_5m_tmax_CanESM5_ssp585_2081-2100.zip", exdir = "worldclim")
-
+unzip("worldclim/wc2.1_5m_elev.zip", exdir = "worldclim")
 
 # import ------------------------------------------------------------------
 
 # raster
 # Presente
-tmax_presente <- raster::stack(dir(path = here::here("worldclim"), pattern = ".tif"))
-tmax_presente_mean <- raster::stack(dir(path = here::here("worldclim"), pattern = ".tif")) %>% 
-  mean()
+tmax_presente <- dir(path = here::here("worldclim"), pattern = "tmax", full.names = TRUE) %>% 
+  grep(".tif", ., value = TRUE) %>% 
+  raster::stack()
+tmax_presente_mean <- tmax_presente %>% mean()
 tmax_presente_mean
-tmax_presente_max <- raster::stack(dir(path = here::here("worldclim"), pattern = ".tif")) %>% 
-  max()
+tmax_presente_max <- tmax_presente %>% max()
+tmax_presente_max
 
+elev <- raster::raster("worldclim/wc2.1_5m_elev.tif")
+elev
 
 # Previsao 1 (2100 cenario de acao)
-tmax_fut_1 <- raster::stack(dir(path = here::here("worldclim/future"), pattern = "CanESM5_ssp245_2081-2100"))
-tmax_fut_1_mean <- raster::stack(dir(path = here::here("worldclim/future"), pattern = "CanESM5_ssp245_2081-2100")) %>% 
+tmax_fut_1 <- raster::stack(here::here("worldclim", "wc2.1_5m_tmax_CanESM5_ssp245_2081-2100.tif"))
+tmax_fut_1_mean <- tmax_fut_1 %>% 
   mean()
 tmax_fut_1_mean
-tmax_fut_1_max <- raster::stack(dir(path = here::here("worldclim/future"), pattern = "CanESM5_ssp245_2081-2100")) %>% 
+tmax_fut_1_max <- tmax_fut_1 %>% 
   max()
 
 
 # Previsao 2 (2100 cenario de continuar aquecendo)
-tmax_fut_2 <- raster::stack(dir(pattern = "CanESM5_ssp585_2081-2100"))
-tmax_fut_2_mean <- raster::stack(dir(pattern = "CanESM5_ssp585_2081-2100")) %>% 
+tmax_fut_2 <- raster::stack(here::here("worldclim", "wc2.1_5m_tmax_CanESM5_ssp585_2081-2100.tif"))
+tmax_fut_2_mean <- tmax_fut_2 %>% 
   mean()
 tmax_fut_2_mean
-tmax_fut_2_max <- raster::stack(dir(pattern = "CanESM5_ssp585_2081-2100")) %>% 
+tmax_fut_2_max <- tmax_fut_2 %>% 
   max()
-
+tmax_fut_2_max
 
 # extrair -----------------------------------------------------------------
+
+# resumo
+ants_sp_sf_values <- ants_sp_sf %>% 
+  dplyr::mutate(tmax_presente_mean = raster::extract(tmax_presente_mean, .),
+                tmax_fut_1_mean = raster::extract(tmax_fut_1_mean, .),
+                tmax_fut_2_mean = raster::extract(tmax_fut_2_mean, .),
+                tmax_presente_max = raster::extract(tmax_presente_max, .),
+                tmax_fut_1_max = raster::extract(tmax_fut_1_max, .),
+                tmax_fut_2_max = raster::extract(tmax_fut_2_max, .),
+                elev = raster::extract(elev, .)) %>% 
+  na.omit()
+ants_sp_sf_values
+
+glimpse(ants_sp_sf_values)
 
 # Media de temperatura maxima ao longo dos meses
 ants_sp_sf_values <- ants_sp_sf %>% 
@@ -176,6 +199,10 @@ ants_sp_sf_values
 
 ants_sp_sf_values <- ants_sp_sf_values %>% 
   dplyr::mutate(tmax_fut_2_mean = raster::extract(tmax_fut_2_mean, .))
+ants_sp_sf_values
+
+ants_sp_sf_values <- ants_sp_sf_values %>% 
+  dplyr::mutate(elev = raster::extract(elev, .))
 ants_sp_sf_values
 
 # Maxima de temperatura maxima no ano todo
@@ -191,6 +218,10 @@ ants_sp_sf_values <- ants_sp_sf_values %>%
   dplyr::mutate(tmax_fut_2_max = raster::extract(tmax_fut_2_max, .))
 ants_sp_sf_values
 
+
+# mapas -------------------------------------------------------------------
+
+
 # Visualizar minimo e maximo (da temp max media ou max total) 
 # para cada spp em um periodo do tempo
 ants_sp_sf_values %>% 
@@ -199,7 +230,8 @@ ants_sp_sf_values %>%
             max = max(tmax_presente_max, na.rm = TRUE))
 
 tm_shape(ants_sp_sf_values) +
-  tm_bubbles(col = "tmax_presente_max", size = .3)
+  tm_bubbles(col = "tmax_presente_max", size = .3, breaks = c(20, 25, 30, 35, 40, 50)) +
+  tm_layout(legend.position = c("left", "bottom"))
 
 tm_shape(ants_sp_sf_values) +
   tm_bubbles(col = "tmax_fut_1_max", size = .3)
@@ -220,16 +252,34 @@ tm_shape(ants_sp_sf_values) +
 ants_sp_sf_values %>% group_by(sp) %>% summarize(occ = n())
 
 # Futuro 1 (numero de locais que a temperatura se manteria aceitavel)
-ants_sp_sf_values %>% group_by(sp) %>% 
+ants_sp_sf_values %>% 
+  group_by(sp) %>% 
   summarize(surv_Ectatomma = sum(tmax_fut_1_max < 40 & sp == "Ectatomma brunneum", na.rm = T),
             surv_Eciton = sum(tmax_fut_1_max < 40 & sp == "Eciton burchellii", na.rm = T),
             surv_Solenopsis = sum(tmax_fut_1_max < 45.3 & sp == "Solenopsis invicta", na.rm = T))
 
 # Futuro 2 (numero de locais que a temperatura se manteria aceitavel)
-ants_sp_sf_values %>% group_by(sp) %>% 
+ants_sp_sf_values %>% 
+  group_by(sp) %>% 
   summarize(surv_Ectatomma = sum(tmax_fut_2_max < 40 & sp == "Ectatomma brunneum", na.rm = T),
             surv_Eciton = sum(tmax_fut_2_max < 40 & sp == "Eciton burchellii", na.rm = T),
             surv_Solenopsis = sum(tmax_fut_2_max < 45.3 & sp == "Solenopsis invicta", na.rm = T))
+
+
+ants_sp_sf_values_eb <- ants_sp_sf_values %>% 
+  sf::st_drop_geometry() %>% 
+  filter(sp == "Ectatomma brunneum") %>% 
+  mutate(sobreviveu_present = ifelse(tmax_presente_max > 40, 0, 1),
+         sobreviveu_fut1 = ifelse(tmax_fut_1_max > 40, 0, 1),
+         sobreviveu_fut2 = ifelse(tmax_fut_2_max > 40, 0, 1)) %>% 
+  dplyr::select(elev, sobreviveu_present, sobreviveu_fut1, sobreviveu_fut2) %>% 
+  pivot_longer(cols = c(sobreviveu_present, sobreviveu_fut1, sobreviveu_fut2), 
+               names_to = "cenario", values_to = "sobrevivencia")
+ants_sp_sf_values_eb
+
+ggplot(ants_sp_sf_values_eb, aes(x = elev, y = sobrevivencia, color = cenario)) +
+  geom_point() +
+  geom_smooth(method = "glm", method.args = list(family = "binomial"))
 
 
 ## biomas (Diego)
@@ -266,3 +316,21 @@ tm_shape(biomas) +
   tm_grid(lines = FALSE, 
           labels.format = list(big.mark = ""), 
           labels.rot = c(0, 90))
+
+
+# mapa com x --------------------------------------------------------------
+
+ants_sp_sf_values_eb <- ants_sp_sf_values %>% 
+  filter(sp == "Ectatomma brunneum") %>% 
+  mutate(sobreviveu_present = ifelse(tmax_presente_max > 40, 0, 1),
+         sobreviveu_fut1 = ifelse(tmax_fut_1_max > 40, 0, 1),
+         sobreviveu_fut2 = ifelse(tmax_fut_2_max > 40, 0, 1)) %>% 
+  dplyr::select(elev, sobreviveu_present, sobreviveu_fut1, sobreviveu_fut2) %>% 
+  tidyr::gather(c(sobreviveu_present, sobreviveu_fut1, sobreviveu_fut2), 
+                key = "cenario", value = "sobrevivencia") %>% 
+  mutate(sobrevivencia = as.factor(sobrevivencia))
+ants_sp_sf_values_eb
+
+tm_shape(ants_sp_sf_values) +
+  tm_symbols(size = .1, shape = "sobrevivencia", shapes = c(20, 4)) +
+  tm_facets("cenario")
